87a9ba39dd65483093c14d839d6427a3368c8dea
mariyahmalik <123036027+mariyahmalik@users.noreply.github.com>
2023-04-02 16:04:15 -0600

    Update TaskTest.java

--- edu/ucalgary/oop/TaskTest.java
+++ edu/ucalgary/oop/TaskTest.java
@@ -11,12 +11,11 @@ package edu.ucalgary.oop;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-import java.beans.Transient;
-
 public class TaskTest {
 
     @Test 
     public void testValidTask() {
+     
         // Create a valid Task object
         Task task = new Task("Task description", 30, 60, 9, 12);
         
@@ -30,6 +29,7 @@ public class TaskTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void testInvalidTaskEndsBeforeBegins() {
+     
         //Create a Task with an invalid window
         Task task = new Task("Task description", 30, 60, 12, 9);
 
@@ -37,24 +37,28 @@ public class TaskTest {
 
     @Test(expected = IllegalArgumentException.class)
     public void testInvalidTaskTooLongForWindow() {
+     
         // Create a Task with an invalid duration
         Task task = new Task ("Task description", 30, 120, 9, 12);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testInvalidTaskWindowStartsAtInvalidHour() {
+     
         // Create a Task with an invalid start hour
         Task task = new Task("Task description", 30, 60, -1, 12);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void tesetInvalidTaskWindowEndsAtInvalidHour() {
+     
         // Create a Task with an invalid end hour
         Task task = new Task ("Task description", 30, 60, 9, 25);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testInvalidTaskNegativePrepTimeOrDuration() {
+     
         // Create a Task with negative prep time
         Task task = new Task ("Task description", -10, 60, 9, 12);
 
